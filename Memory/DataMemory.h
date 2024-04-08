82dd1579543f337aa13bd0b7ddbe1f9e1f9c6b72
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-04-08 12:19:18 +0200

    added Memory files

--- Memory/DataMemory.h
+++ Memory/DataMemory.h
@@ -1,8 +1,57 @@
-//
-// Created by ENG-Mohamed on 4/7/2024.
-//
-
 #ifndef PROCESSOR_DESIGN_DATAMEMORY_H
 #define PROCESSOR_DESIGN_DATAMEMORY_H
+
+#include <stdio.h>
+#define WORD_SIZE 8
+#define DATA_MEM_SIZE 2024
+
+struct Word {
+    uint8_t size ;
+};
+
+// Array of words
+struct DataMemory {
+    Word Dmemory[DATA_MEM_SIZE];
+};
+
+/**
+ * Initializing DM with values
+ * @param mem : refrence to the DM
+ */
+void IMInit(DataMemory *mem){
+    for (int i = 0; i < DATA_MEM_SIZE ; ++i) {
+        mem->Dmemory[i] = 0 ;
+    }
+}
+
+/**
+ * Reading from DM
+ * @param mem : refrence to the DM
+ * @param address : address is 16 bit because DATA_MEM_SIZE =2024 So
+ *                  log(2024) =10.9 --> 16
+ */
+uint16_t IMRead(DataMemory *mem, uint16_t address){
+    if(address >= DATA_MEM_SIZE){
+        printf("This Address not in Data Memory range of Addresses");
+        return -1 ;
+    }
+    return mem->Dmemory[address];
+}
+/**
+ * Writing To DM
+ * @param mem : refrence to the DM
+ * @param address : address is 16 bit because DATA_MEM_SIZE =2024 So
+ *                  log(2024) =10.9 --> 16
+ * @param data : data is 16 bit because WORD_SIZE = 16
+ */
+void IMWrite(DataMemory *mem, uint16_t address, uint16_t data){
+    if(address >= DATA_MEM_SIZE){
+        printf("This Address not in Data Memory range of Addresses");
+        return  ;
+    }
+    mem->Dmemory[address] = data ;
+}
+
+
 
 #endif //PROCESSOR_DESIGN_DATAMEMORY_H
--- Memory/InstructionMemory.h
+++ Memory/InstructionMemory.h
@@ -1,8 +1,56 @@
-//
-// Created by ENG-Mohamed on 4/7/2024.
-//
-
 #ifndef PROCESSOR_DESIGN_INSTRUCTIONMEMORY_H
 #define PROCESSOR_DESIGN_INSTRUCTIONMEMORY_H
+
+#include <stdio.h>
+#define WORD_SIZE 16
+#define INSTRUCTION_MEM_SIZE 1024
+
+struct Word {
+    uint16_t size ;
+};
+
+// Array of words
+struct InstructionMemory {
+    Word Imemory[INSTRUCTION_MEM_SIZE];
+};
+
+/**
+ * Initializing IM with values
+ * @param mem : refrence to the IM
+ */
+void IMInit(InstructionMemory *mem){
+    for (int i = 0; i < INSTRUCTION_MEM_SIZE ; ++i) {
+        mem->Imemory[i] = 0 ;
+    }
+}
+
+/**
+ * Reading from IM
+ * @param mem : refrence to the IM
+ * @param address : address is 16 bit because INSTRUCTION_MEM_SIZE =1024 So
+ *                  log(1024) =10 --> 16
+ */
+uint16_t IMRead(InstructionMemory *mem, uint16_t address){
+    if(address >= INSTRUCTION_MEM_SIZE){
+        printf("This Address not in Instruction Memory range of Addresses") ;
+        return -1 ;
+    }
+    return mem->Imemory[address];
+}
+/**
+ * Writing To IM
+ * @param mem : refrence to the IM
+ * @param address : address is 16 bit because INSTRUCTION_MEM_SIZE =1024 So
+ *                  log(1024) =10 --> 16
+ * @param data : data is 16 bit because WORD_SIZE = 16
+ */
+void IMWrite(InstructionMemory *mem, uint16_t address, uint16_t data){
+    if(address >= INSTRUCTION_MEM_SIZE){
+        printf("This Address not in Instruction Memory range of Addresses") ;
+        return  ;
+    }
+    mem->Imemory[address] = data ;
+}
+
 
 #endif //PROCESSOR_DESIGN_INSTRUCTIONMEMORY_H
