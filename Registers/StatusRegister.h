9aa7e5d0e5b688259afabf623650136bba147f48
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-04-09 16:56:39 +0200

    Aywa Ba2a👌👌👍👍🤷‍♂️🤷‍♂️

--- Registers/StatusRegister.h
+++ Registers/StatusRegister.h
@@ -3,21 +3,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#include <stdint.h>
+
 typedef struct  {
-    unsigned char flags; // to represent all flags in one byte
-    static const unsigned char C_FLAG = 0b00000001; // Carry Flag
-    static const unsigned char V_FLAG = 0b00000010; // Two's Complement Overflow Flag
-    static const unsigned char N_FLAG = 0b00000100; // Negative Flag
-    static const unsigned char S_FLAG = 0b00001000; // Sign Flag
-    static const unsigned char Z_FLAG = 0b00010000; // Zero Flag
-}SREG;
+    uint8_t C : 1 ;
+    uint8_t V : 1 ;
+    uint8_t N : 1 ;
+    uint8_t S : 1 ;
+    uint8_t Z : 1 ;
+    uint8_t rest : 3 ;
+} SREG;
 
 /**
  * Initialize Status Register
  * @param sreg : pointer to the Status Register
  */
 void sreg_init(SREG *sreg) {
-    sreg->flags = 0;
+    sreg->C = 0;
+    sreg->V = 0;
+    sreg->N = 0;
+    sreg->S = 0;
+    sreg->Z = 0;
+    sreg->rest = 0;
 }
 
 /**
@@ -26,10 +33,7 @@ void sreg_init(SREG *sreg) {
  * @param value : value to set the carry flag (0 or 1)
  */
 void setC_FLAG(SREG *sreg, uint8_t value) {
-    if (value)
-        sreg->flags |= SREG::C_FLAG; // bitwise OR
-    else
-        sreg->flags &= ~SREG::C_FLAG; // bitwise AND ~: not
+    sreg->C = value;
 }
 
 /**
@@ -38,10 +42,7 @@ void setC_FLAG(SREG *sreg, uint8_t value) {
  * @param value : value to set the two's complement overflow flag (0 or 1)
  */
 void setV_FLAG(SREG *sreg, uint8_t value) {
-    if (value)
-        sreg->flags |= SREG::V_FLAG;
-    else
-        sreg->flags &= ~SREG::V_FLAG;
+    sreg->V = value;
 }
 
 /**
@@ -50,10 +51,7 @@ void setV_FLAG(SREG *sreg, uint8_t value) {
  * @param value : value to set the negative flag (0 or 1)
  */
 void setN_FLAG(SREG *sreg, uint8_t value) {
-    if (value)
-        sreg->flags |= SREG::N_FLAG;
-    else
-        sreg->flags &= ~SREG::N_FLAG;
+    sreg->N = value;
 }
 
 /**
@@ -61,10 +59,7 @@ void setN_FLAG(SREG *sreg, uint8_t value) {
  * @param sreg : pointer to the Status Register
  */
 void setS_FLAG(SREG *sreg) {
-    if ((sreg->flags & SREG::N_FLAG) ^ (sreg->flags & SREG::V_FLAG))
-        sreg->flags |= SREG::S_FLAG;
-    else
-        sreg->flags &= ~SREG::S_FLAG;
+    sreg->S = (sreg->N ^ sreg->V);
 }
 
 /**
@@ -73,12 +68,8 @@ void setS_FLAG(SREG *sreg) {
  * @param value : value to set the zero flag (0 or 1)
  */
 void setZ_FLAG(SREG *sreg, uint8_t value) {
-    if (value)
-        sreg->flags |= SREG::Z_FLAG;
-    else
-        sreg->flags &= ~SREG::Z_FLAG;
+    sreg->Z = value;
 }
-
 
 
 
