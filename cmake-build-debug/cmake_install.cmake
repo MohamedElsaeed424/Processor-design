3e3ee8cd185071abf3c1cf43ba096b803ab26146
Mahmoud Dahroug <mahmoudyasser2002@gmail.com>
2024-05-17 22:13:30 +0300

    clean up

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/Coding/C_Programing/Processor-design
+# Install script for directory: C:/Users/Mahmoud/CLionProjects/Processor-design
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Coding/C_Programing/Processor-design/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Mahmoud/CLionProjects/Processor-design/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
